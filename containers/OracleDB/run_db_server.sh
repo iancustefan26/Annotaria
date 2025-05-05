7cbe398394d9505ed5a4c023e6a9b54238c60479
TudorIsCoding <tudor2016tudor@outlook.com>
2025-05-05 15:10:28 +0300

    first relevant commit : create a docker container for oracle db and script for creating the user, and initiating the project with some plugins

---
+++ containers/OracleDB/run_db_server.sh
@@ -0,0 +1,20 @@
+docker run --name webDB -d -p 1523:1521 -e ORACLE_PASSWORD=api_test gvenzl/oracle-free
+
+# here copy scripts for populating after creating them
+
+docker cp create_user.sql webDB:/tmp/
+
+echo "Waiting for Oracle to be ready..."
+until docker logs webDB 2>&1 | grep -q "DATABASE IS READY TO USE"; do
+  sleep 2
+done
+echo "Oracle is ready!"
+
+# here copy execute the scripts for populating the database inside docker image
+#
+
+docker exec -it webDB sqlplus sys/api_test as sysdba @/tmp/create_user.sql
+sleep 1
+
+echo "For more info about the docker image: https://hub.docker.com/r/gvenzl/oracle-free"
+echo "Connect with api_test/api_test"
