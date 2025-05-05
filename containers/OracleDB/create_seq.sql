cda8309c6a27acb2717db23044063cf47b4dc96c
TudorIsCoding <tudor2016tudor@outlook.com>
2025-05-05 15:36:26 +0300

    scrip for creating user table and for creating user sequence

---
+++ containers/OracleDB/create_seq.sql
@@ -0,0 +1,9 @@
+drop sequence seq_user_id;
+
+commit;
+
+create sequence seq_user_id start with 1 increment by 1 minvalue 1 nomaxvalue;
+
+commit;
+
+exit;
--- containers/OracleDB/run_db_server.sh
+++ containers/OracleDB/run_db_server.sh
@@ -3,6 +3,8 @@ docker run --name webDB -d -p 1523:1521 -e ORACLE_PASSWORD=api_test gvenzl/oracl
 # here copy scripts for populating after creating them
 
 docker cp create_user.sql webDB:/tmp/
+docker cp create_seq.sql webDB:/tmp/ 
+docker cp create_tables.sql webDB:/tmp/ 
 
 echo "Waiting for Oracle to be ready..."
 until docker logs webDB 2>&1 | grep -q "DATABASE IS READY TO USE"; do
@@ -15,6 +17,11 @@ echo "Oracle is ready!"
 
 docker exec -it webDB sqlplus sys/api_test as sysdba @/tmp/create_user.sql
 sleep 1
+docker exec -it webDB sqlplus api_test/api_test @/tmp/create_tables.sql 
+sleep 1 
+docker exec -it webDB sqlplus api_test/api_test @/tmp/create_seq.sql
+sleep 1
+
 
 echo "For more info about the docker image: https://hub.docker.com/r/gvenzl/oracle-free"
 echo "Connect with api_test/api_test"
