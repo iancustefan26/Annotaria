51c5b4a7a07da1f14fa3ce7a7bd1e54c95214322
Iancu Stefan-Teodor <stefaniancu46@gmail.com>
2025-05-28 12:54:55 +0300

    Graph and feed formulas tested and working

--- containers/OracleDB/page_rank.sql
+++ containers/OracleDB/page_rank.sql
@@ -30,34 +30,48 @@ AS
         v_graph graph := graph();
         v_author1_id NUMBER;
         v_author2_id NUMBER;
+        v_row float_array;
     BEGIN
         SELECT p.id BULK COLLECT INTO v_post_ids FROM POST p 
         WHERE p.author_id IN (
             SELECT * FROM (SELECT user2_id FROM FRIENDSHIP WHERE user1_id = p_user_id) WHERE ROWNUM < p_best_friends_number
             );
+
         v_post_rank_score := float_array();
         FOR i in v_post_ids.first..v_post_ids.last LOOP
-            v_post_rank_score(i):= feed_fromulas.score(p_user_id, v_post_ids(i));
+            v_post_rank_score.EXTEND;
+            v_post_rank_score(v_post_rank_score.COUNT) := feed_formulas.score(p_user_id, v_post_ids(i));
         END LOOP;
         FOR i IN v_post_ids.first..v_post_ids.last LOOP
+            v_row := float_array();
             FOR j IN v_post_ids.first..v_post_ids.last LOOP
+                v_row.EXTEND;
                 IF i = j THEN
-                    v_graph(i)(j) := v_post_rank_score(i);
+                    v_row(j) := v_post_rank_score(i);
+                    --v_graph(i)(j) := v_post_rank_score(i);
                 ELSE
                     SELECT author_id INTO v_author1_id FROM POST WHERE id = v_post_ids(i);
                     SELECT author_id INTO v_author2_id FROM POST WHERE id = v_post_ids(j);
                     IF v_author1_id = v_author2_id THEN
-                        v_graph(i)(j) := 1;
-                        v_graph(j)(i) := 1;
+                        v_row(j) := 1;
+                        --v_graph(i)(j) := 1;
+                        --v_graph(j)(i) := 1;
                     ELSE 
-                        v_graph(i)(j) := 0;
-                        v_graph(j)(i) := 0;
+                        v_row(j) := 0;
+                        --v_graph(i)(j) := 0;
+                        --v_graph(j)(i) := 0;
                     END IF;
                 END IF;
             END LOOP;
+            v_graph.EXTEND;
+            v_graph(i) := v_row;
         END LOOP;
+        RETURN v_graph;
+        EXCEPTION
+            WHEN OTHERS THEN
+                DBMS_OUTPUT.PUT_LINE('Backtrace     : ' || DBMS_UTILITY.FORMAT_ERROR_BACKTRACE);
+                RAISE;
     END generate;
-
 END graph_generator;
 
 /
